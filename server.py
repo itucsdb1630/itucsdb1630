021c1370eee86a1d73fcebeba223e5fc4ca5c988
Emre Eroğlu <emreeroglu@itu.edu.tr>
2016-10-07 17:52:52 +0300

    Elephant Sql settings added.

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import datetime
 import os
+import json
+import re
 
 from flask import Flask
 from flask import render_template
@@ -14,10 +16,41 @@ def home_page():
     return render_template('home.html', current_time=now.ctime())
 
 
+#@emreeroglu
+#for connecting project to elephant sql
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
+
+
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
-    app.run(host='0.0.0.0', port=port, debug=debug)
+
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = "user='{}' password='{}' host='{}' port={} dbname='{}'".format(
+            os.getenv('DBUSER', 'vagrant'),
+            os.getenv('DBPASS', 'vagrant'),
+            os.getenv('DBHOST', 'localhost'),
+            os.getenv('DBPORT', '54321'),
+            os.getenv('DBNAME', 'itucsdb')
+        )
+
+    app.run(host='127.0.0.1', port=port, debug=debug)
+
+
+
+
+
