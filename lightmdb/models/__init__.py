52cfb49bf3de883d857326aed0123fe5e6e8c253
memduh <memduhg@gmail.com>
2016-11-29 16:55:15 +0300

    playlist_movie object added

--- lightmdb/models/__init__.py
+++ lightmdb/models/__init__.py
@@ -2,6 +2,8 @@ from .common import Database
 from .user import User, Follower
 from .contactus import ContactMessage
 from .movie import Movie
+from .playlist import Playlist
+from .playlist_movie import Playlist_Movie
 
 __all__ = [
     'Database',
@@ -9,4 +11,6 @@ __all__ = [
     'Follower',
     'ContactMessage',
     'Movie',
+    'Playlist',
+    'Playlist_Movie'
 ]
